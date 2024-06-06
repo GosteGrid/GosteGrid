c4483dcfb0f543c1e009b9b9ddc6044b63cbf7d5
GosteGrid <andrey.mireev@mail.ru>
2024-06-06 12:18:45 +0300

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -9,16 +9,13 @@ gifs = [
 # Выбор случайного видео
 chosen_gif = random.choice(gifs)
 
-# Формирование URL видео для отображения в README
-gifs_url = f'https://raw.githubusercontent.com/GosteGrid/GosteGride/main/gif/{chosen_gif}'
-
 # Создание содержимого README.md
 readme_content = f"""
 # Привет!
 
 Добро пожаловать в мой профиль! Вот случайное видео для вас:
 
-![Видео]({gifs_url})
+![Видео](gif/{chosen_gif})
 """
 
 # Запись содержимого в README.md

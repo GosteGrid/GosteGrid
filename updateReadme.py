852f7da7829625dd9de5e338046fa50094a86120
GosteGrid <andrey.mireev@mail.ru>
2024-06-06 15:26:08 +0300

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -1,16 +1,26 @@
-import random
+import os
 
-# Список гифок
+# Путь к папке с гифками
+gif_folder = "gif"
+
+# Список гифок в нужном порядке
 gif_list = [
-    "gif/test.gif",
-    "gif/test2.gif"
+    "test.gif",
+    "test2.gif"
 ]
 
-# Выбор случайной гифки
-random_gif = random.choice(gif_list)
+# Название последней использованной гифки
+last_gif = "test2.gif"
+
+# Поиск индекса последней использованной гифки в списке
+last_index = gif_list.index(last_gif)
+
+# Выбор следующей гифки по порядку
+next_index = (last_index + 1) % len(gif_list)
+next_gif = gif_list[next_index]
 
 # Формирование ссылки на гифку
-gif_link = f"![Random GIF]({random_gif})"
+gif_link = f"![Random GIF]({gif_folder}/{next_gif})"
 
 # Обновление файла README
 with open("README.md", "r+") as readme_file:

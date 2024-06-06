d664bfb432375bed75b187e8d3c46b89ab683406
GosteGrid <andrey.mireev@mail.ru>
2024-06-06 16:16:28 +0300

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -1,30 +1,35 @@
 import os
+import random
 
 # Путь к папке с гифками
-gif_folder = "gif"
+gif_dir = 'gif'
 
-# Список гифок в нужном порядке
-gif_list = [
+# Получение списка файлов с расширением .gif
+gifs = [
     "test.gif",
-    "test2.gif", 
+    "test2.gif",
     "test3.gif"
 ]
 
-# Путь к файлу README
-readme_file_path = "README.md"
+# Выбор случайной гифки
+chosen_gif = random.choice(gifs)
 
-# Чтение текущей гифки из файла README
-with open(readme_file_path, "r") as readme_file:
-    readme_content = readme_file.read()
-    current_gif_index = (gif_list.index(readme_content.split("](")[1].split("/")[1]) + 1) if gif_list[-1] not in readme_content else 0
+# Получение индекса выбранной гифки
+gif_index = gifs.index(chosen_gif)
 
-# Выбор следующей гифки по порядку
-next_index = current_gif_index % len(gif_list)
-next_gif = gif_list[next_index]
+# Получение следующей гифки (с учетом цикличности)
+next_gif_index = (gif_index + 1) % len(gifs)
+next_gif = gifs[next_gif_index]
 
-# Формирование ссылки на гифку
-gif_link = f"![Random GIF]({gif_folder}/{next_gif})"
+# Формирование содержимого README.md с новой гифкой
+readme_content = f"""
+# Привет!
 
-# Обновление файла README
-with open(readme_file_path, "w") as readme_file:
-    readme_file.write(gif_link)
+Добро пожаловать в мой профиль! Вот случайная гифка для вас:
+
+![Гифка](gifs/{next_gif})
+"""
+
+# Запись содержимого в README.md
+with open('README.md', 'w') as file:
+    file.write(readme_content)

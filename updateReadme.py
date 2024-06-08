b1ab6d4c8305b4923c59ba60ccf6b3618f78cdfe
GosteGrid <andrey.mireev@mail.ru>
2024-06-09 00:51:13 +0300

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -39,6 +39,8 @@ next_gif = gifs[next_gif_index]
 readme_content = f"""
 ![Гифка](video_2024-06-08_23-36-04.gif)
 
+I am a 1st year student receiving a bachelor's degree in software engineering at the Polytechnic University.  I enjoy working with different technologies and trying myself in different areas, as well as learning something new.  Currently, I am developing simple programs and implementing various data structures, which allows me to understand the internal structure of the structures and the principle of their operation.
+
 Добро пожаловать в мой профиль! Вот случайная гифка для вас:
 
 ![Гифка](gif/{next_gif})

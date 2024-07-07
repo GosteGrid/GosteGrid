d63f88d85cde5df3ab5f427e0510a166f7564994
GosteGrid <andrey.mireev@mail.ru>
2024-07-07 20:12:15 +0300

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -49,14 +49,13 @@ I'm a 1st year student pursuing a Bachelor's degree in Software Engineering at t
     <img src="https://img.shields.io/badge/C++-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white" alt="C++">
     <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" alt="Python">
     <img src="https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white" alt="Git">
-
-    
 </div>
+![Гифка](gif/{next_gif})
+
+
 
 <div align="center">
-    <h2><img src="./img/car2.png" alt="car2" width="24"> Technology Stack <img src="./img/car1.png" alt="car1" width="24"> </h2>
-
-    
+    <h2><img src="./img/car2.png" alt="car2" width="64"> Technology Stack <img src="./img/car1.png" alt="car1" width="64"> </h2>
 </div>
 
 ![Гифка](gif/{next_gif})

a350ccf55e4abea1d9dab8f7fc6eb9d1be708116
GosteGrid <andrey.mireev@mail.ru>
2024-07-17 16:49:52 +0300

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -59,6 +59,7 @@ I'm a 1st year student pursuing a Bachelor's degree in Software Engineering at t
 
 [![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=GosteGrid&show_icons=true&theme=outrun#gh-dark-mode-only)](https://github.com/GosteGrid/github-readme-stats#gh-dark-mode-only)
 [![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=GosteGrid&show_icons=true&theme=swift&bg_color=00000000#gh-light-mode-only)](https://github.com/GosteGrid/github-readme-stats#gh-light-mode-only)
+[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=GosteGrid&repo=test)](https://github.com/GosteGrid/test)
 
 """
 

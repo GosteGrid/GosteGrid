e20ea657f7b00325720115444abeae48c3adc91e
GosteGrid <andrey.mireev@mail.ru>
2024-07-30 00:20:25 +0300

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -50,16 +50,39 @@ I'm a 1st year student pursuing a Bachelor's degree in Software Engineering at t
     <img src="./maket2.png" alt="Python" width="100">
     <img src="./maket3.png" alt="Git" width="100">
 </div>
+
 <a href="javascript:void(0)"> <br> ![Гифка](gif/test2.gif) </a>
+
 <div align="center">
     <h2><img src="./redMan.gif" alt="Redman" width="32"> My GitHub Stats <img src="./redMan.gif" alt="Redman" width="32"></h2>
 </div>
+
 <div align="center">
   <a href="https://github.com/GosteGrid/github-readme-stats">
     <picture>
       <source media="(prefers-color-scheme: dark)" srcset="https://github-readme-stats.vercel.app/api?username=GosteGrid&show=reviews&show_icons=true&theme=midnight-purple&include_all_commits=true&bg_color=00000000#gh-dark-mode-only">
       <source media="(prefers-color-scheme: light)" srcset="https://github-readme-stats.vercel.app/api?username=GosteGrid&show=reviews&show_icons=true&include_all_commits=true&theme=graywhite&bg_color=00000000#gh-light-mode-only">
       <img src="https://github-readme-stats.vercel.app/api?username=GosteGrid&show=reviews&show_icons=true&theme=default&include_all_commits=true" alt="Anurag's GitHub stats">
+    </picture>
+  </a>
+</div>
+&nbsp; &nbsp;
+<div align="center">
+  <a href="https://github.com/GosteGrid/github-readme-stats">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="https://github-readme-stats.vercel.app/api/top-langs/?username=GosteGrid&layout=donut&theme=midnight-purple&bg_color=00000000#gh-dark-mode-only">
+      <source media="(prefers-color-scheme: light)" srcset="https://github-readme-stats.vercel.app/api/top-langs/?username=GosteGrid&layout=donut&theme=graywhite&bg_color=00000000#gh-light-mode-only">
+      <img src="https://github-readme-stats.vercel.app/api?username=GosteGrid&show=reviews&show_icons=true&theme=default" alt="Anurag's GitHub stats">
+    </picture>
+  </a>
+</div>
+&nbsp; &nbsp;
+<div align="center">
+  <a href="https://git.io/streak-stats">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="https://streak-stats.demolab.com?user=GosteGrid&theme=midnight-purple&background=00000000&hide_border=00000000">
+      <source media="(prefers-color-scheme: light)" srcset="https://streak-stats.demolab.com?user=GosteGrid&theme=graywhite&background=00000000&hide_border=00000000">
+      <img src="https://streak-stats.demolab.com/?username=GosteGrid&theme=default" alt="GitHub Streak">
     </picture>
   </a>
 </div>
